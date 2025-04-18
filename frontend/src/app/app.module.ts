e8b17ff30dd20de59e8e5d0cd4fda92e3edfb1d3
Alvaropin17 <alvaro17pc@gmail.com>
2025-04-18 22:58:23 +0200

    Creation of template module and refactor of files

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,11 +7,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
 import { HttpClientModule } from '@angular/common/http';
+import { EditorComponent } from './templates/editor/editor.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditorComponent
   ],
   imports: [
     BrowserModule,
