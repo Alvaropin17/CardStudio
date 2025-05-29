c95da64697f156b1d2192b07a256d79ce073ab56
Alvaropin17 <alvaro17pc@gmail.com>
2025-05-29 13:24:06 +0200

    Fix

--- backend/src/database/models/cardImage.js
+++ backend/src/database/models/cardImage.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
-const Template = require('./template');
 
 const cardImageSchema = new mongoose.Schema({
-  userId: { type: Number, required: true },
-  templateId: { type: Number, required: true }, 
-  csvId: { type: Number, required: true },
+  userId: { type: Number, required: true },     
   name: { type: String, required: true },
   base64: { type: String, required: true },
 });
