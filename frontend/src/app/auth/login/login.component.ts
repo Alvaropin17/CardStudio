6c6e831dd857316bfeae845144cf88f87c399283
Alvaropin17 <alvaro17pc@gmail.com>
2025-06-06 18:51:47 +0200

    Automatic redirect after login

--- frontend/src/app/auth/login/login.component.ts
+++ frontend/src/app/auth/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -13,7 +14,7 @@ export class LoginComponent {
   loginForm: FormGroup;
   message: string | null = null;
 
-  constructor(private fb: FormBuilder,private authService: AuthService) {
+  constructor(private fb: FormBuilder,private authService: AuthService, private router: Router) {
     this.loginForm = this.fb.group({
       user: ['', Validators.required],
       password: ['', Validators.required]
@@ -28,10 +29,8 @@ export class LoginComponent {
     const { user, password } = this.loginForm.value;
 
     this.authService.login(user, password).subscribe({
-      next: (response) => {
-        this.message = 'Login exitoso 🎉';
-        localStorage.setItem('user', JSON.stringify(response.body));
-        console.log('Respuesta:', response);
+      next: () => {
+        this.router.navigate(['profile']);
       },
       error: (error) => {
         this.message = `Error: ${error.message}`;
